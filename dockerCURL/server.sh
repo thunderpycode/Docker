97baa04b364c167601bd48ac77a888521c2b0268
AbhishekMac <abhi.sri784@gmail.com>
2024-05-26 19:34:36 +0530

    finalvideodone

--- dockerCURL/server.sh
+++ dockerCURL/server.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!bin/sh
 while true; do
-    echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n$(cat /index.html)" | nc -l -p 90
+    echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\n$(cat \index.html)" | nc -l -p 90
 done
